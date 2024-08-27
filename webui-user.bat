6e9a2ea44e872a23980e956d52e3c5eeda43061f
3wardfish <edwardfish1@hotmail.co.uk>
2024-08-27 15:03:11 +0100

    Update configuration and add new scripts

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Program Files\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS= --xformers --disable-safe-unpickle
+git pull
 call webui.bat
